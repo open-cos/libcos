6b64b7600e7524ae94a21d0601f7fd9d518bec34
David Luco <5156878+dluco@users.noreply.github.com>
2024-01-17 23:01:37 -0800

    Implement more array operations

--- include/libcos/common/CosArray.h
+++ include/libcos/common/CosArray.h
@@ -14,8 +14,6 @@
 COS_DECLS_BEGIN
 COS_ASSUME_NONNULL_BEGIN
 
-typedef struct CosArray CosArray;
-
 typedef void (*CosArrayRetainItemCallback)(void *item);
 typedef void (*CosArrayReleaseItemCallback)(void *item);
 typedef bool (*CosArrayEqualItemsCallback)(const void *item1,
@@ -37,10 +35,80 @@ cos_array_alloc(size_t element_size,
 void
 cos_array_free(CosArray *array);
 
+#pragma mark - Accessors
+
+/**
+ * @brief Gets the number of items in an array.
+ *
+ * @param array The array.
+ *
+ * @return The number of items in the array.
+ */
+size_t
+cos_array_get_count(const CosArray *array);
+
+/**
+ * @brief Gets the capacity of an array.
+ *
+ * @param array The array.
+ *
+ * @return The capacity of the array.
+ */
+size_t
+cos_array_get_capacity(const CosArray *array);
+
+/**
+ * @brief Gets the item at a given index in an array.
+ *
+ * @param array The array.
+ * @param index The index of the item to get.
+ * @param error On input, a pointer to an error object, or @c NULL.
+ *
+ * @return The item at the given index, or @c NULL if an error occurred.
+ */
+void * COS_Nullable
+cos_array_get_item(const CosArray *array,
+                   size_t index,
+                   CosError * COS_Nullable error);
+
+#pragma mark Insertion
+
 bool
-cos_array_append(CosArray *array,
-                 const void *item,
-                 CosError * COS_Nullable error);
+cos_array_insert_item(CosArray *array,
+                      size_t index,
+                      void *item,
+                      CosError * COS_Nullable error);
+
+bool
+cos_array_append_item(CosArray *array,
+                      void *item,
+                      CosError * COS_Nullable error);
+
+bool
+cos_array_insert_items(CosArray *array,
+                       size_t index,
+                       void *items,
+                       size_t count,
+                       CosError * COS_Nullable error);
+
+bool
+cos_array_append_items(CosArray *array,
+                       void *items,
+                       size_t count,
+                       CosError * COS_Nullable error);
+
+#pragma mark Removal
+
+bool
+cos_array_remove_item(CosArray *array,
+                      size_t index,
+                      CosError * COS_Nullable error);
+
+bool
+cos_array_remove_items(CosArray *array,
+                       size_t index,
+                       size_t count,
+                       CosError * COS_Nullable error);
 
 COS_ASSUME_NONNULL_END
 COS_DECLS_END
--- src/common/CosArray.c
+++ src/common/CosArray.c
@@ -7,7 +7,8 @@
 #include "common/Assert.h"
 #include "common/CosUtils.h"
 
-#include <limits.h>
+#include <libcos/common/CosError.h>
+
 #include <stdlib.h>
 #include <string.h>
 
@@ -22,6 +23,13 @@ struct CosArray {
     CosArrayCallbacks callbacks;
 };
 
+/**
+ * @brief Rounds up the capacity to the next power of 2.
+ *
+ * @param capacity The capacity to be rounded.
+ *
+ * @return The rounded capacity.
+ */
 static size_t
 cos_round_capacity_(size_t capacity)
 {
@@ -29,12 +37,33 @@ cos_round_capacity_(size_t capacity)
         return 4;
     }
     // Round up to the next power of 2.
-    const size_t next_capacity = 1UL << cos_flsl((long)capacity);
-    if (next_capacity > LONG_MAX) {
-        return LONG_MAX;
-    }
-    return next_capacity;
+    return cos_next_pow2l(capacity + 1);
 }
+
+static void
+cos_apply_func_(void *items,
+                size_t count,
+                size_t element_size,
+                void (*func)(void *item));
+
+static bool
+cos_array_insert_items_(CosArray *array,
+                        size_t index,
+                        void *items,
+                        size_t count,
+                        CosError * COS_Nullable error);
+
+static bool
+cos_array_remove_items_(CosArray *array,
+                        size_t index,
+                        size_t count,
+                        CosError * COS_Nullable error);
+
+static bool
+cos_array_resize_(CosArray *array,
+                  size_t required_capacity);
+
+#pragma mark - Public
 
 CosArray *
 cos_array_alloc(size_t element_size,
@@ -100,10 +129,57 @@ cos_array_free(CosArray *array)
     free(array);
 }
 
+#pragma mark - Accessors
+
+size_t
+cos_array_get_count(const CosArray *array)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    if (!array) {
+        return 0;
+    }
+
+    return array->count;
+}
+
+size_t
+cos_array_get_capacity(const CosArray *array)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    if (!array) {
+        return 0;
+    }
+
+    return array->capacity;
+}
+
+void *
+cos_array_get_item(const CosArray *array,
+                   size_t index,
+                   CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    if (!array) {
+        return NULL;
+    }
+
+    if (index >= array->count) {
+        COS_ERROR_PROPAGATE(cos_error_make(COS_ERROR_INVALID_ARGUMENT,
+                                           "Index out of bounds"),
+                            error);
+        return NULL;
+    }
+
+    return (array->data + (index * array->element_size));
+}
+
+#pragma mark Insertion
+
 bool
-cos_array_append(CosArray *array,
-                 const void *item,
-                 CosError * COS_Nullable error)
+cos_array_insert_item(CosArray *array,
+                      size_t index,
+                      void *item,
+                      CosError * COS_Nullable error)
 {
     COS_PARAMETER_ASSERT(array != NULL);
     COS_PARAMETER_ASSERT(item != NULL);
@@ -111,24 +187,244 @@ cos_array_append(CosArray *array,
         return false;
     }
 
-    const size_t count = array->count;
-    const size_t capacity = array->capacity;
-    if (count + 1 >= capacity) {
-        const size_t new_capacity = cos_round_capacity_(capacity * 2);
-        unsigned char * const new_data = realloc(array->data,
-                                                 new_capacity * array->element_size);
-        if (!new_data) {
-            return false;
-        }
+    return cos_array_insert_items(array, index, item, 1, error);
+}
 
-        array->data = new_data;
-        array->capacity = new_capacity;
+bool
+cos_array_append_item(CosArray *array,
+                      void *item,
+                      CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    COS_PARAMETER_ASSERT(item != NULL);
+    if (!array) {
+        return false;
     }
 
-    memmove(array->data + (count * array->element_size),
-            item,
-            array->element_size);
-    array->count = count + 1;
+    // Insert the item at the end of the array.
+    return cos_array_insert_items_(array, array->count, item, 1, error);
+}
+
+bool
+cos_array_insert_items(CosArray *array,
+                       size_t index,
+                       void *items,
+                       size_t count,
+                       CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    COS_PARAMETER_ASSERT(items != NULL);
+
+    return cos_array_insert_items_(array, index, items, count, error);
+}
+
+bool
+cos_array_append_items(CosArray *array,
+                       void *items,
+                       size_t count,
+                       CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    COS_PARAMETER_ASSERT(items != NULL);
+    if (!array) {
+        return false;
+    }
+
+    // Insert the items at the end of the array.
+    return cos_array_insert_items_(array, array->count, items, count, error);
+}
+
+#pragma mark Removal
+
+bool
+cos_array_remove_item(CosArray *array,
+                      size_t index,
+                      CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+
+    return cos_array_remove_items_(array, index, 1, error);
+}
+
+bool
+cos_array_remove_items(CosArray *array,
+                       size_t index,
+                       size_t count,
+                       CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+
+    return cos_array_remove_items_(array, index, count, error);
+}
+
+#pragma mark - Private
+
+static void
+cos_apply_func_(void *items,
+                size_t count,
+                size_t element_size,
+                void (*func)(void *item))
+{
+    COS_PARAMETER_ASSERT(items != NULL);
+    COS_PARAMETER_ASSERT(func != NULL);
+    if (!items || !func) {
+        return;
+    }
+
+    if (count == 0 || element_size == 0) {
+        // Nothing to do.
+        return;
+    }
+
+    unsigned char * const data = items;
+    for (size_t i = 0; i < count; i++) {
+        void * const item = data + (i * element_size);
+        func(item);
+    }
+}
+
+static bool
+cos_array_insert_items_(CosArray *array,
+                        size_t index,
+                        void *items,
+                        size_t count,
+                        CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    COS_PARAMETER_ASSERT(items != NULL);
+    if (!array || !items) {
+        return false;
+    }
+
+    if (count == 0) {
+        // Nothing to do.
+        return true;
+    }
+
+    // Ensure that the index is within the bounds of the array.
+    const size_t current_count = array->count;
+    if (index > current_count) {
+        COS_ERROR_PROPAGATE(cos_error_make(COS_ERROR_INVALID_ARGUMENT,
+                                           "Index out of bounds"),
+                            error);
+        return false;
+    }
+
+    // Ensure that the array has enough capacity to hold the new items.
+    const size_t required_capacity = current_count + count;
+    if (required_capacity > array->capacity && !cos_array_resize_(array, required_capacity)) {
+        COS_ERROR_PROPAGATE(cos_error_make(COS_ERROR_MEMORY,
+                                           "Failed to resize array"),
+                            error);
+        return false;
+    }
+
+    const size_t element_size = array->element_size;
+    unsigned char * const data = array->data;
+
+    // Retain the new items.
+    CosArrayRetainItemCallback retain_callback = array->callbacks.retain;
+    if (retain_callback) {
+        cos_apply_func_(items, count, element_size, retain_callback);
+    }
+
+    // We only need to shift existing items if we're inserting in the middle of the array.
+    if (index < current_count) {
+        // Shift the items after the insertion point to the end of the insertion range.
+        memmove(data + ((index + count) * element_size),
+                data + (index * element_size),
+                (current_count - index) * element_size);
+    }
+
+    // Copy the new items into the insertion range.
+    memcpy(data + (index * element_size),
+           items,
+           count * element_size);
+    array->count += count;
+
+    return true;
+}
+
+static bool
+cos_array_remove_items_(CosArray *array,
+                        size_t index,
+                        size_t count,
+                        CosError * COS_Nullable error)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    if (!array) {
+        return false;
+    }
+
+    if (count == 0) {
+        // Nothing to do.
+        return true;
+    }
+
+    // Ensure that the index is within the bounds of the array.
+    const size_t current_count = array->count;
+    if (index >= current_count) {
+        COS_ERROR_PROPAGATE(cos_error_make(COS_ERROR_INVALID_ARGUMENT,
+                                           "Index out of bounds"),
+                            error);
+        return false;
+    }
+
+    const size_t element_size = array->element_size;
+    unsigned char * const data = array->data;
+
+    const size_t removal_byte_offset = index * element_size;
+    const size_t removal_byte_count = count * element_size;
+
+    void * const removal_start = data + removal_byte_offset;
+
+    // Release the removed items.
+    const CosArrayReleaseItemCallback release_callback = array->callbacks.release;
+    if (release_callback) {
+        void * const items = removal_start;
+        cos_apply_func_(items, count, element_size, release_callback);
+    }
+
+    const size_t trailing_item_count = current_count - (index + count);
+
+    // We only need to shift existing items if we're removing from the middle of the array.
+    if (trailing_item_count > 0) {
+        const void * const trailing_items = (data + removal_byte_offset) + removal_byte_count;
+        // Shift the items after the removal point to the start of the removal range.
+        memmove(removal_start,
+                trailing_items,
+                trailing_item_count * element_size);
+    }
+
+    array->count -= count;
+
+    return true;
+}
+
+static bool
+cos_array_resize_(CosArray *array,
+                  size_t required_capacity)
+{
+    COS_PARAMETER_ASSERT(array != NULL);
+    if (!array) {
+        return false;
+    }
+
+    if (array->capacity >= required_capacity) {
+        // No need to resize.
+        return true;
+    }
+
+    const size_t new_capacity = cos_round_capacity_(required_capacity);
+
+    unsigned char * const new_data = realloc(array->data,
+                                             new_capacity * array->element_size);
+    if (!new_data) {
+        return false;
+    }
+
+    array->data = new_data;
+    array->capacity = new_capacity;
 
     return true;
 }
