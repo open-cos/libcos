c15dd7d254d3105bb1c6068ca644774a8e141ddb
David Luco <5156878+dluco@users.noreply.github.com>
2025-02-06 17:16:16 -0800

    Add documentation

--- src/syntax/tokenizer/CosTokenizer.h
+++ src/syntax/tokenizer/CosTokenizer.h
@@ -22,14 +22,36 @@ void
 cos_tokenizer_destroy(CosTokenizer *tokenizer)
     COS_DEALLOCATOR_FUNC;
 
+/**
+ * @brief Creates a new tokenizer.
+ *
+ * @param input_stream The input stream to tokenize.
+ *
+ * @return The new tokenizer, or @c NULL if an error occurred.
+ */
 CosTokenizer * COS_Nullable
 cos_tokenizer_create(CosStream *input_stream)
     COS_ALLOCATOR_FUNC
-    COS_ALLOCATOR_FUNC_MATCHED_DEALLOC(cos_tokenizer_destroy);
+    COS_ALLOCATOR_FUNC_MATCHED_DEALLOC(cos_tokenizer_destroy)
+    COS_OWNERSHIP_HOLDS(1);
 
+/**
+ * @brief Resets the tokenizer to an initial state.
+ *
+ * @param tokenizer The tokenizer to reset.
+ */
 void
 cos_tokenizer_reset(CosTokenizer *tokenizer);
 
+/**
+ * @brief Gets the next token from the tokenizer.
+ *
+ * @param tokenizer The tokenizer.
+ * @param out_token A pointer to the variable in which to store the token.
+ * @param out_error The error object to set if an error occurs.
+ *
+ * @return @c true if a token was successfully read, @c false otherwise.
+ */
 bool
 cos_tokenizer_get_next_token(CosTokenizer *tokenizer,
                              CosToken *out_token,
