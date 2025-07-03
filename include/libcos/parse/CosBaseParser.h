0899fb9597b266cf97842a6f1b3004d4d2637922
David Luco <dluco@open-cos.org>
2025-07-02 17:45:55 -0700

    Add token handling APIs to CosBaseParser

--- include/libcos/parse/CosBaseParser.h
+++ include/libcos/parse/CosBaseParser.h
@@ -15,12 +15,18 @@ COS_DECLS_BEGIN
 COS_ASSUME_NONNULL_BEGIN
 
 struct CosBaseParser {
+    /**
+     * The allocator used by the parser.
+     */
+    CosAllocator *allocator;
+
     CosDoc *doc;             ///< The document being parsed.
     CosStream *input_stream; ///< The input stream to read from.
     CosTokenizer *tokenizer; ///< The tokenizer used for parsing.
 
-    CosToken *token_buffer; ///< Buffer for tokens.
-    size_t token_count;     ///< Number of tokens in the buffer.
+    CosToken *token_buffer;   ///< Buffer for tokens.
+    size_t token_buffer_size; ///< Size of the token buffer.
+    size_t token_count;       ///< Number of tokens in the buffer.
 
     CosDiagnosticHandler * COS_Nullable diagnostic_handler; ///< Handler for diagnostics.
 };
@@ -31,6 +37,69 @@ cos_base_parser_init(CosBaseParser *parser,
                      CosStream *input_stream)
     COS_OWNERSHIP_HOLDS(3)
     COS_WARN_UNUSED_RESULT;
+
+COS_API void
+cos_base_parser_destroy(CosBaseParser *parser);
+
+/**
+ * @brief Gets the parser's current token.
+ *
+ * @param parser The parser.
+ *
+ * @return The current token, or @c NULL if there is no current token.
+ */
+COS_API CosToken * COS_Nullable
+cos_base_parser_get_current_token(CosBaseParser *parser);
+
+/**
+ * @brief Checks if there is a next token.
+ *
+ * @param parser The parser.
+ *
+ * @return @c true if there is a next token, otherwise @c false.
+ */
+COS_API bool
+cos_base_parser_has_next_token(CosBaseParser *parser);
+
+/**
+ * @brief Peeks a next token without consuming it.
+ *
+ * Additional tokens are read to fill the buffer up to the specified lookahead index.
+ *
+ * @param parser The parser.
+ * @param lookahead The lookahead index.
+ *
+ * @return The peeked next token, or @c NULL if there is no next token.
+ */
+COS_API CosToken * COS_Nullable
+cos_base_parser_peek_next_token(CosBaseParser *parser,
+                                unsigned int lookahead);
+
+/**
+ * @brief Advances the parser to the next token.
+ *
+ * This function advances the parser to the next token by shifting the token buffer,
+ * releasing the current token. Additional tokens are *not* read to re-fill the buffer.
+ *
+ * @param parser The parser to be advanced.
+ */
+COS_API void
+cos_base_parser_advance(CosBaseParser *parser);
+
+/**
+ * @brief Checks if the next token matches the specified type.
+ *
+ * @param parser The parser to check.
+ * @param type The token type to match.
+ * @param out_error Optional output parameter for errors.
+ *
+ * @return @c true if the next token matches the specified type, otherwise @c false.
+ */
+COS_API bool
+cos_base_parser_matches_next_token(CosBaseParser *parser,
+                                   CosToken_Type type,
+                                   CosError * COS_Nullable out_error)
+    COS_ATTR_ACCESS_WRITE_ONLY(3);
 
 COS_ASSUME_NONNULL_END
 COS_DECLS_END
--- src/parse/CosBaseParser.c
+++ src/parse/CosBaseParser.c
@@ -10,6 +10,8 @@
 #include "libcos/CosDoc.h"
 #include "libcos/common/memory/CosMemory.h"
 
+#include <string.h>
+
 COS_ASSUME_NONNULL_BEGIN
 
 bool
@@ -25,6 +27,9 @@ cos_base_parser_init(CosBaseParser *parser,
     }
 
     CosAllocator * const allocator = cos_doc_get_allocator(document);
+    if (!allocator) {
+        return false;
+    }
 
     CosTokenizer *tokenizer = NULL;
     CosToken *token_buffer = NULL;
@@ -40,12 +45,15 @@ cos_base_parser_init(CosBaseParser *parser,
         goto failure;
     }
 
+    parser->allocator = allocator;
+
     parser->doc = document;
     parser->input_stream = input_stream;
 
     parser->tokenizer = tokenizer;
 
     parser->token_buffer = token_buffer;
+    parser->token_buffer_size = 3; // Initial size of the token buffer.
     parser->token_count = 0;
     parser->diagnostic_handler = cos_doc_get_diagnostic_handler(document);
 
@@ -59,6 +67,133 @@ failure:
         cos_free(allocator, token_buffer);
     }
     return false;
+}
+
+void
+cos_base_parser_destroy(CosBaseParser *parser)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return;
+    }
+
+    // Release all the buffered tokens.
+    for (size_t i = 0; i < parser->token_count; i++) {
+        CosToken * const token = &(parser->token_buffer[i]);
+        cos_token_reset(token);
+    }
+
+    cos_tokenizer_destroy(parser->tokenizer);
+
+    cos_free(parser->allocator, parser);
+}
+
+CosToken * COS_Nullable
+cos_base_parser_get_current_token(CosBaseParser *parser)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return NULL;
+    }
+
+    return cos_base_parser_peek_next_token(parser, 0);
+}
+
+bool
+cos_base_parser_has_next_token(CosBaseParser *parser)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return false;
+    }
+
+    const CosToken *token = cos_base_parser_peek_next_token(parser, 0);
+    return (token != NULL);
+}
+
+CosToken *
+cos_base_parser_peek_next_token(CosBaseParser *parser,
+                                unsigned int lookahead)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return NULL;
+    }
+
+    if (lookahead >= parser->token_buffer_size) {
+        return NULL;
+    }
+
+    // Fill up the buffer up to the requested lookahead index.
+    for (unsigned int i = 0; i <= lookahead; i++) {
+        if (i >= parser->token_count) {
+            CosToken *token = &(parser->token_buffer[i]);
+
+            if (!cos_tokenizer_get_next_token(parser->tokenizer,
+                                              token,
+                                              NULL)) {
+                return NULL;
+            }
+
+            parser->token_count++;
+        }
+    }
+
+    CosToken * const peeked_token = &parser->token_buffer[lookahead];
+    COS_ASSERT(peeked_token != NULL, "Expected a token");
+    return peeked_token;
+}
+
+void
+cos_base_parser_advance(CosBaseParser *parser)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return;
+    }
+
+    if (parser->token_count == 0) {
+        return;
+    }
+
+    // Release the current token.
+    CosToken *current_token = &(parser->token_buffer[0]);
+    cos_token_reset(current_token);
+
+    const size_t token_buffer_size = parser->token_buffer_size;
+
+    // "Pop" the first token by moving the rest of the buffer left with memmove.
+    memmove(&parser->token_buffer[0],
+            &parser->token_buffer[1],
+            sizeof(parser->token_buffer[0]) * (token_buffer_size - 1));
+
+    // Zero out the last token after the shift.
+    memset(&(parser->token_buffer[token_buffer_size - 1]),
+           0,
+           sizeof(CosToken));
+
+    // Decrement the token count.
+    parser->token_count--;
+}
+
+bool
+cos_base_parser_matches_next_token(CosBaseParser *parser,
+                                   CosToken_Type type,
+                                   CosError * COS_Nullable out_error)
+{
+    COS_API_PARAM_CHECK(parser != NULL);
+    if (COS_UNLIKELY(!parser)) {
+        return false;
+    }
+
+    CosToken * const token = cos_base_parser_peek_next_token(parser, 0);
+    if (!token) {
+        return false;
+    }
+
+    (void)out_error;
+
+    return (token->type == type);
 }
 
 COS_ASSUME_NONNULL_END
