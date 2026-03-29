7b6cb6e8d2ca83f082bbd5357215c23be1869da4
David Luco <dluco@open-cos.org>
2026-03-28 19:07:04 -0700

    Fix get_test_name variable usage

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -1,5 +1,5 @@
 #[[
-    get_test_name(TEST_NAME TEST_SOURCE)
+    get_test_name(OUT_VAR TEST_SOURCE)
 
     Derives a test name from a source-file path by combining the directory
     and the filename (without extension) with a "/" separator.
@@ -8,22 +8,22 @@
         unit-tests/tokenizer.c  ->  unit-tests/tokenizer
         parser.c                ->  parser
 ]]
-macro(get_test_name TEST_NAME TEST_SOURCE)
-    get_filename_component(TEST_DIRNAME
+macro(get_test_name OUT_VAR TEST_SOURCE)
+    get_filename_component(_GTN_DIRNAME
         ${TEST_SOURCE}
         # Directory of the test source file.
         DIRECTORY
     )
 
-    get_filename_component(TEST_BASENAME
+    get_filename_component(_GTN_BASENAME
         ${TEST_SOURCE}
         # File name without directory and last extension.
         NAME_WLE
     )
 
-    if (TEST_DIRNAME)
-        set(TEST_NAME "${TEST_DIRNAME}/${TEST_BASENAME}")
+    if (_GTN_DIRNAME)
+        set(${OUT_VAR} "${_GTN_DIRNAME}/${_GTN_BASENAME}")
     else ()
-        set(TEST_NAME "${TEST_BASENAME}")
+        set(${OUT_VAR} "${_GTN_BASENAME}")
     endif ()
 endmacro()
